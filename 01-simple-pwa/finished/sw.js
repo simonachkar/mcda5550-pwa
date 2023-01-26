23d670625a6991fb8cec252ca8ddf5a81f98964c
Simon Achkar <simon@simonachkar.com>
2023-01-26 15:56:06 -0400

    01-simple-pwa - Added more info and instructions in `start/`

--- 01-simple-pwa/finished/sw.js
+++ 01-simple-pwa/finished/sw.js
@@ -35,6 +35,7 @@ self.addEventListener('fetch', function (event) {
     event.respondWith(
         caches.match(event.request)
             .then(function (res) {
+                console.log('Fetched:', res);
                 return res;
             })
     );
