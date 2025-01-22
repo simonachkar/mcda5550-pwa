012cf998a95f411d1252b1e4e7a6fb76f7e5eb74
simonachkar <simon@simonachkar.com>
2025-01-22 13:43:03 -0400

    Added `04-react-pwa`, created PWA with React + Vite, some initial documentation

---
+++ 04-react-pwa/vite.config.js
@@ -0,0 +1,51 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      },
+      manifest: {
+        name: "PWA with React",
+        short_name: "react-pwa",
+        icons: [
+          {
+            src: "/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "/pwa-maskable-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "/pwa-maskable-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+        start_url: "/",
+        display: "standalone",
+        background_color: "#E0EBAF",
+        theme_color: "#71D94E",
+        description: "PWA app created with React and Vite for MCDA5550",
+      },
+    }),
+  ],
+});
